bc4dcd1f3ca30600f4ca68a8970fb4fe5d2f4572
Rohan2519Y <rohankumar17645@gmail.com>
2025-09-09 21:01:58 +0530

    Update About.js

--- portfolio_frontend/src/components/About.js
+++ portfolio_frontend/src/components/About.js
@@ -3,8 +3,8 @@ import Aos from "aos";
 import "aos/dist/aos.css";
 export default function About() {
     useEffect(() => {
-    Aos.init({ once: false }); 
-  }, []);
+        Aos.init({ once: false });
+    }, []);
     return (<>
         <div className="w-full flex justify-center items-center h-screen bg-[linear-gradient(to_right,_#ffdde1,_#ee9ca7)] 
         dark:from-stone-900 dark:to-stone-800 dark:bg-[linear-gradient(to_right,_#2c2c2c,_#4a4a4a)]">
@@ -13,10 +13,14 @@ export default function About() {
                     <div data-aos="fade-right"
                         data-aos-duration="800"
                         className="w-full h-24 text-7xl font-bold text-stone-900 dark:text-stone-100">About</div>
-                    <div className="w-full h-80 text-gray-700 dark:text-gray-300 text-5xl font-semibold tracking-wider leading-snug">I’m a passionate <span className="font-bold text-rose-600 dark:text-rose-400">Full-Stack Developer</span> who loves turning complex ideas into elegant, user-friendly web applications. With a focus on <span className="font-bold text-rose-600 dark:text-rose-400">React</span>, <span className="font-bold text-rose-600 dark:text-rose-400">Node.js</span>, and modern <span className="font-bold text-rose-600 dark:text-rose-400">JavaScript</span>, I enjoy building fast, scalable solutions and continuously exploring new technologies to stay ahead.</div>
+                    <div data-aos="fade-right"
+                        data-aos-duration="800"
+                        className="w-full h-80 text-gray-700 dark:text-gray-300 text-5xl font-semibold tracking-wider leading-snug">I’m a passionate <span className="font-bold text-rose-600 dark:text-rose-400">Full-Stack Developer</span> who loves turning complex ideas into elegant, user-friendly web applications. With a focus on <span className="font-bold text-rose-600 dark:text-rose-400">React</span>, <span className="font-bold text-rose-600 dark:text-rose-400">Node.js</span>, and modern <span className="font-bold text-rose-600 dark:text-rose-400">JavaScript</span>, I enjoy building fast, scalable solutions and continuously exploring new technologies to stay ahead.</div>
                 </div>
-                <div className="w-[30%] h-full flex justify-center items-center">
-                    <img className="w-full" src="https://res.cloudinary.com/dio6iadsq/image/upload/v1757144496/Programmer-PNG-Free-File-Download_cdwton.png" />
+                <div className="w-[30%] h-full flex justify-center items-center overflow-hidden">
+                    <img data-aos="fade-left"
+                        data-aos-duration="800"
+                        className="w-full" src="https://res.cloudinary.com/dio6iadsq/image/upload/v1757144496/Programmer-PNG-Free-File-Download_cdwton.png" />
                 </div>
             </div>
         </div>
